95ce77607bb77db5839a57de8c1790a0fcd1f1a1
João <joao_gabriel@live.com.pt>
2022-04-30 23:34:52 -0300

    configurando db

--- src/localConfigs/databaseConfigs.js
+++ src/localConfigs/databaseConfigs.js
@@ -17,6 +17,10 @@ const configs = {
       "ba8198375f7da174b031ad746898bdb759d9c3027c4edabf22fab2730213da29",
     database: "d89amta8ks05ik",
   },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 module.exports = configs;
