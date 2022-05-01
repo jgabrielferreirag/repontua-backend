c2cc3355efdb573b0c222b31ce3e8df99cd3867c
João <joao_gabriel@live.com.pt>
2022-04-30 22:41:23 -0300

    script

---
+++ src/localConfigs/databaseConfigs.js
@@ -0,0 +1,11 @@
+const configs = {
+  client: "pg",
+  connection: {
+    host: "localhost",
+    user: "postgres",
+    password: "653255",
+    database: "provihack",
+  },
+};
+
+module.exports = configs;
