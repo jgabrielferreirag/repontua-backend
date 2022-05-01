010c18017701167e58f827a34da84786855d5c45
João Gabriel Ferreira Germano dos Santos <67394387+jgabrielferreirag@users.noreply.github.com>
2022-05-01 18:46:44 -0300

    Update databaseConfigs.js

--- src/localConfigs/databaseConfigs.js
+++ src/localConfigs/databaseConfigs.js
@@ -1,11 +1,11 @@
 const configs = {
   client: "pg",
   connection: {
-    host: "ec2-54-164-40-66.compute-1.amazonaws.com",
-    user: "aobcsporhbsdqr",
+    host: "##############",
+    user: "#############",
     password:
-      "ba8198375f7da174b031ad746898bdb759d9c3027c4edabf22fab2730213da29",
-    database: "d89amta8ks05ik",
+      "#################",
+    database: "#############",
     port: 5432,
   },
   ssl: {
