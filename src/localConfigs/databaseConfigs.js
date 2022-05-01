00d6ecffa3677f7032e7abb2ae89b835c9c170db
João <joao_gabriel@live.com.pt>
2022-04-30 23:52:28 -0300

    database

--- src/localConfigs/databaseConfigs.js
+++ src/localConfigs/databaseConfigs.js
@@ -16,8 +16,8 @@ const configs = {
     password:
       "ba8198375f7da174b031ad746898bdb759d9c3027c4edabf22fab2730213da29",
     database: "d89amta8ks05ik",
+    port: 5432,
   },
-  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
