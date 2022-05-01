87824f792c8fbffae353b7ede59428228db64142
João <joao_gabriel@live.com.pt>
2022-04-30 23:02:57 -0300

    heroku database

--- src/localConfigs/databaseConfigs.js
+++ src/localConfigs/databaseConfigs.js
@@ -1,10 +1,21 @@
-const configs = {
+/* const configs = {
   client: "pg",
   connection: {
     host: "localhost",
     user: "postgres",
     password: "653255",
     database: "provihack",
+  },
+};
+ */
+const configs = {
+  client: "pg",
+  connection: {
+    host: "ec2-54-164-40-66.compute-1.amazonaws.com",
+    user: "aobcsporhbsdqr",
+    password:
+      "ba8198375f7da174b031ad746898bdb759d9c3027c4edabf22fab2730213da29",
+    database: "d89amta8ks05ik",
   },
 };
 
